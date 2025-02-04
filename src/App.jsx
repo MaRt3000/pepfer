99a48b8c4100b80faf971af189571da7a7660786
MaRt <ogbemart@gmail.com>
2025-02-04 18:47:40 +0100

    made changes on the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import "@mantine/core/styles.css";
 
-import { MantineProvider } from "@mantine/core";
 import ScrollToTop from "./components/ScrollToTop";
 import Faq from "./components/Faq";
 import Footer from "./components/Footer";
@@ -17,27 +15,23 @@ import Empowering from "./components/Empowering";
 
 export default function App() {
   return (
-    <MantineProvider>
-      {
-        <Router>
-          <ScrollToTop />
-          <div className="max-w-[1440px] mx-auto">
-            <Nav />
+    <Router>
+      <ScrollToTop />
+      <div className="max-w-[1440px] mx-auto">
+        <Nav />
 
-            <Routes>
-              <Route path="" element={<LandingPage />} />
-              <Route path="home" element={<LandingPage />} />
-              <Route path="what-we-offer" element={<WhatWeOffer />} />
-              <Route path="who-do-we-serve" element={<WhoDoWeServe />} />
-              <Route path="features" element={<Empowering />} />
-              <Route path="terms-of-use" element={<TermsOfUse />} />
-              <Route path="privacy-policy" element={<PrivacyPolicy />} />
-            </Routes>
-            <Faq />
-            <Footer />
-          </div>
-        </Router>
-      }
-    </MantineProvider>
+        <Routes>
+          <Route path="" element={<LandingPage />} />
+          <Route path="home" element={<LandingPage />} />
+          <Route path="what-we-offer" element={<WhatWeOffer />} />
+          <Route path="who-do-we-serve" element={<WhoDoWeServe />} />
+          <Route path="features" element={<Empowering />} />
+          <Route path="terms-of-use" element={<TermsOfUse />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
+        <Faq />
+        <Footer />
+      </div>
+    </Router>
   );
 }
